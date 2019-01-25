53dcdc9f4e9391c98c4e711ada16bbfd0521a7e7
DK\pfi <pfi@sitecore.net>
2019-01-25 11:08:01 -0500

    Initial Commit

---
+++ src/Sitecore.Support.306662/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.306662")]
+[assembly: AssemblyProduct("Sitecore.Support.306662")]
+[assembly: ComVisible(false)]
